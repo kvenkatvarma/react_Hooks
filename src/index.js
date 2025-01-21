5238df7215e1ab5f3c8a53bac318d8ab2bab0c25
Venkat varma <kvenkatvarma@gmail.com>
2025-01-21 14:01:55 +0530

    Added files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Note the change here
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import "font-awesome/css/font-awesome.css"
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App></App>
+  
+);
