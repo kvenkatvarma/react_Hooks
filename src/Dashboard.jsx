fad65421c3a9fe0e79a871e9be77624e7a907bde
Venkat varma <kvenkatvarma@gmail.com>
2025-01-22 19:57:59 +0530

    Reusable Service

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,13 +1,8 @@
 import React,{useEffect,useContext,useState} from "react";
 import { UserContext } from "./UserContext";
 import Orders from "./Order";
+import {OrderService,ProductsService} from "./Service";
 
-let previousOrders =(orders)=>{
-  return orders.filter(ord=>ord.isPaymentCompleted === true);
-};
-let getCart =(orders)=>{
-   return orders.filter(ord=>ord.isPaymentCompleted === false);
- };
 function Dashboard(){
    let userContext = useContext(UserContext);
 useEffect(()=>{
@@ -21,12 +16,14 @@ useEffect(()=>{
         
 
          //get products
-         let productsResponse = await fetch(`http://localhost:5000/products`,{method:"GET"});
+         let productsResponse = await ProductsService.getProducts();
          if(productsResponse.ok)
          {
             let prodbody =  await productsResponse.json();
             body.forEach((order)=>{
-              order.product = prodbody.find((prod)=>prod.id == order.productId);
+               console.log("Processing order:", order); // Log the order
+               console.log("Processing prodbody:", prodbody); // Log the order
+              order.product = ProductsService.getProductByProductId(prodbody,order.productId) ;
             })
             setOrders(body);
          }
@@ -50,10 +47,10 @@ useEffect(()=>{
              <div className="row">            
               <div className="col-lg-6">
                 <h4 className="py-2 my-2 text-info border-bottom border-info"><i className="fa fa-history"></i> Pevious Orders{" "}
-                <span className="badge badge-info">{previousOrders(orders).length}</span>
+                <span className="badge badge-info">{OrderService.getpreviousOrders(orders).length}</span>
                 </h4>
-                {previousOrders(orders).length == 0 ? (<div className="text-danger">No Orders</div>):("") }
-                {previousOrders(orders).map((ord)=>{
+                {OrderService.getpreviousOrders(orders).length == 0 ? (<div className="text-danger">No Orders</div>):("") }
+                {OrderService.getpreviousOrders(orders).map((ord)=>{
                   return <Orders key={ord.id} orderId ={ord.id} productId = {ord.productId} userId = {ord.userId} isPaymentCompleted = {ord.isPaymentCompleted} quantity = {ord.quantity}
                   productName = {ord.product.productName} price={ord.product.price}/>
                 })}
@@ -61,10 +58,10 @@ useEffect(()=>{
 
               <div className="col-lg-6">
                 <h4 className="py-2 my-2 text-primary border-bottom border-primary"><i className="fa fa-shopping-cart"></i> Cart{" "} 
-                <span className="badge badge-primary">{getCart(orders).length}</span>
+                <span className="badge badge-primary">{OrderService.getCart(orders).length}</span>
                 </h4>
-                {getCart(orders).length == 0 ? (<div className="text-danger">No Products in cart</div>):("") }
-                {getCart(orders).map((ord)=>{
+                {OrderService.getCart(orders).length == 0 ? (<div className="text-danger">No Products in cart</div>):("") }
+                {OrderService.getCart(orders).map((ord)=>{
                   return <Orders key={ord.id} orderId ={ord.id} productId = {ord.productId} userId = {ord.userId} isPaymentCompleted = {ord.isPaymentCompleted} quantity = {ord.quantity}
                   productName = {ord.product.productName} price={ord.product.price}/>
                 })}
