780b018b1a91d2e688aebcb411c1c0fb724f6398
Venkat varma <kvenkatvarma@gmail.com>
2025-01-23 17:10:05 +0530

    Products Grid

--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,5 +1,86 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import {ProductsService,CategoryService,BrandsService} from "./Service";
 function ProductsList(props){
-  return <div>Products</div>
+    let [search,setSearch] = useState("");
+    let [products,setProducts] = useState([]);
+   
+
+    useEffect(()=>{
+        (async()=>{
+            let brandsService = await BrandsService.fetchBrands();
+            let brandsbody = await brandsService.json();
+
+            let categoryService = await CategoryService.fetchCategories();
+            let categorybody = await categoryService.json();
+
+
+           let productsResponse = await ProductsService.getProducts();
+           let body = await productsResponse.json();
+            
+           body.forEach(product=>{
+               product.category= CategoryService.getCategoryByCategoryId(categorybody,product.categoryId);
+               product.brand= BrandsService.getBrandByBrandId(brandsbody,product.brandId);
+
+           });
+
+           setProducts(body);
+        })();
+        
+    },[]);
+  return <div className="row">
+       <div className="col-12">
+        <div className="row p-3 header">
+              <div className="col-lg-3">
+                  <h4>
+                    <i className="fa fa-suitcase"></i>Products {" "}
+                    <span className="badge badge-secondary">{products.length}</span>
+                  </h4>
+              </div>
+              <div className="col-lg-9">
+                   <input type="search" placeholder="Search" className="form-control" autoFocus value={search} onChange={(event)=>{
+                          setSearch(event.target.value);
+                   }}></input>
+              </div>
+        </div>
+       </div>
+       <div className="col-lg-10 mx-auto mb-2">
+          <div className="card my-2 shadow">
+             <div className="card-body">
+                 <table className="table">
+                   <thead>
+                       <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Brand</th>
+                        <th>Category</th>
+                        <th>Rating</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       {products.map(product=>{
+                         return <tr key={product.id}>
+                                    <td>{product.id}</td>
+                                    <td>{product.productName}</td>
+                                    <td>{product.price}</td>
+                                    <td>{product.brand.brandName}</td>
+                                    <td>{product.category.categoryName}</td>
+                                    <td>
+                                        {[...Array(product.rating).keys()].map(n=>(
+                                        <i className="fa fa-star text-warning" key={n}></i>
+                                    ))}
+                                    
+                                    {[...Array(5-product.rating).keys()].map(n=>(
+                                        <i className="fa fa-star-o text-warning" key={n}></i>
+                                    ))}
+                                    </td>
+                              </tr>
+                       })}
+                   </tbody>
+                 </table>
+             </div>
+          </div>
+       </div>
+  </div>
 }
 export default ProductsList;
