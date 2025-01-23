74a8c51b36f4cadace75627f65717ad03f99c275
Venkat varma <kvenkatvarma@gmail.com>
2025-01-23 12:19:10 +0530

    useCallback example

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,6 +5,9 @@ import {OrderService,ProductsService} from "./Service";
 
 function Dashboard(){
    let userContext = useContext(UserContext);
+   let [showOrderDeletedAlert,setShowOrderDeletedAlert]= useState(false);
+   let [showOrderPlacedAlert,setShowORderPlacedAlert]= useState(false);
+
 useEffect(()=>{
       document.title ="Dashboard-eCommerce";  
       loadDataFromDatabase();
@@ -45,7 +48,38 @@ useEffect(()=>{
     },[userContext.user.currentUserId]);
   let[orders,setOrders] = useState([]); 
  
+  //buy now click
+  let onBuyNowClick =useCallback(async(orderId,userId,productId,quantity)=>{
+    if(window.confirm("Do you want to place order for this product"))
+    {
+       let updateOrder ={
+         id:orderId,productId:productId,userId:userId,quantity:quantity,isPaymentCompleted:true
+       };
+       let orderResponse = await fetch(`http://localhost:5000/orders/${orderId}`,{method:"PUT",body:JSON.stringify(updateOrder),headers:{
+         "Content-type":"application/json"
+       }});
+       let orderResponseBody = await orderResponse.json();
+       if(orderResponse.ok){
+         console.log(orderResponseBody);
+         loadDataFromDatabase();
+         setShowORderPlacedAlert(true);
+       }
+    }
+  },[loadDataFromDatabase]);
 
+  //Delete button logic
+  let onDeleteclick =useCallback(async(orderId)=>{
+   if(window.confirm("Are you sure to delete this item from cart?"))
+   {
+      let orderResponse = await fetch(`http://localhost:5000/orders/${orderId}`,{method:"DELETE"});
+      if(orderResponse.ok)
+      {
+         let orderResponseBody = await orderResponse.json();
+         setShowOrderDeletedAlert(true);
+         loadDataFromDatabase();
+      }
+   }
+  },[loadDataFromDatabase]);
     return (
          <div className="row">
             <div className="col-12 py-3 header">
@@ -61,7 +95,7 @@ useEffect(()=>{
                 {OrderService.getpreviousOrders(orders).length == 0 ? (<div className="text-danger">No Orders</div>):("") }
                 {OrderService.getpreviousOrders(orders).map((ord)=>{
                   return <Orders key={ord.id} orderId ={ord.id} productId = {ord.productId} userId = {ord.userId} isPaymentCompleted = {ord.isPaymentCompleted} quantity = {ord.quantity}
-                  productName = {ord.product.productName} price={ord.product.price}/>
+                  productName = {ord.product.productName} price={ord.product.price} onBuyNowClick={onBuyNowClick} onDeleteclick={onDeleteclick}/>
                 })}
               </div>
 
@@ -69,10 +103,26 @@ useEffect(()=>{
                 <h4 className="py-2 my-2 text-primary border-bottom border-primary"><i className="fa fa-shopping-cart"></i> Cart{" "} 
                 <span className="badge badge-primary">{OrderService.getCart(orders).length}</span>
                 </h4>
+                {showOrderPlacedAlert ? 
+                 <div className="col-12">
+                <div className="alert alert-success alert-dismissible fade show mt-1" role="alert">Your order has been placed successfully
+                  <button className="close" type="button" data-dismiss="alert">
+                     <span>&times;</span>
+                  </button>
+                </div></div> : "" }
+
+                {showOrderDeletedAlert ? 
+                  <div className="col-12">
+                <div className="alert alert-danger alert-dismissible fade show mt-1" role="alert">Your item has been removed from the cart
+                  <button className="close" type="button" data-dismiss="alert">
+                     <span>&times;</span>
+                  </button>
+                </div> </div> : "" }
+
                 {OrderService.getCart(orders).length == 0 ? (<div className="text-danger">No Products in cart</div>):("") }
                 {OrderService.getCart(orders).map((ord)=>{
                   return <Orders key={ord.id} orderId ={ord.id} productId = {ord.productId} userId = {ord.userId} isPaymentCompleted = {ord.isPaymentCompleted} quantity = {ord.quantity}
-                  productName = {ord.product.productName} price={ord.product.price}/>
+                  productName = {ord.product.productName} price={ord.product.price} onBuyNowClick={onBuyNowClick} onDeleteclick={onDeleteclick}/>
                 })}
               </div>
 
--- src/Order.jsx
+++ src/Order.jsx
@@ -5,7 +5,21 @@ function Orders(props){
     return (
         <div className="card my-2 shadow">
             <div className="card-body">
-                 <h6><i className="fa fa-arrow-right"></i> {props.productName}</h6>
+                 <h6><i className="fa fa-arrow-right"></i> {props.productName}
+                  {props.isPaymentCompleted == false ? <div className="float-right">
+                    <button className="btn btn-sm btn-info mr-2" onClick={()=>{
+                        props.onBuyNowClick(props.orderId,props.userId,props.productId,props.quantity);
+                    }}>
+                        <i className="fa fa-truck"></i>Buy Now
+                    </button>
+                    <button className="btn btn-sm btn-danger mr-2" onClick={()=>{
+                        props.onDeleteclick(props.orderId);
+                    }}>
+                        <i className="fa fa-trash-o"></i>Delete
+                    </button>
+                  </div> : ""}
+                  
+                 </h6>
                  <table className="table table-sm table-borderless mt-1">
                     <tbody>
                         <tr>
